34b726fd49b2a1a5c7a335a2ab82fd165444ac5e
Jordan Dukart <jordan@discoverygarden.ca>
2014-06-11 21:46:58 +0000

    JS fun times with Adam and onDrop.

---
+++ js/islandora_serial_object_tabledrag.js
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Fire events on dragging yay.
+ */
+(function ($) {
+    Drupal.behaviors.updateDragValues = {
+        attach: function(context, settings) {
+            if (typeof settings.islandoraSerialObjectModifiedIntermediates == "undefined") {
+                settings.islandoraSerialObjectModifiedIntermediates = [];
+            }
+            else {
+                console.log(settings.islandoraSerialObjectModifiedIntermediates);
+                $.each(settings.islandoraSerialObjectModifiedIntermediates, function(index, value) {
+                    $(':input[name="' + value + '"]').addClass('islandora-serial-object-modified-intermediate');
+                });
+            }
+            if (Drupal.tableDrag['islandora-serial-object-intermediate-table']) {
+                Drupal.tableDrag['islandora-serial-object-intermediate-table'].onDrop = function() {
+                    var dropped_row = this.rowObject;
+                    var intermediate_levels = settings.islandoraSerialObjectIntermediateLevels;
+                    var indent = this.rowObject.indents + 1;
+                    if (indent in intermediate_levels) {
+                        $(this.oldRowElement).find('input.islandora-serial-object-caption').not('.islandora-serial-object-modified-intermediate').val(intermediate_levels[indent].caption);
+                    }
+                    $(this.rowObject.children).each(function (index, element) {
+                       var indent = $(".indentation", element).length + 1;
+                        if (indent in intermediate_levels) {
+                            $(element).find('input.islandora-serial-object-caption').not('.islandora-serial-object-modified-intermediate').val(intermediate_levels[indent].caption);
+                        }
+                    });
+                }
+            }
+            $('input.islandora-serial-object-caption').keyup(function(object) {
+               $(object.target).addClass('islandora-serial-object-modified-intermediate');
+               if (settings.islandoraSerialObjectModifiedIntermediates.indexOf(object.target.name) == -1) {
+                   settings.islandoraSerialObjectModifiedIntermediates.push(object.target.name);
+               }
+            });
+        }
+    }
+})(jQuery);
