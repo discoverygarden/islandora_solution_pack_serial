adb36c0a59fde1dffa195f3742daf50219f55155
Jordan Dukart <jordan@discoverygarden.ca>
2014-07-08 18:27:09 +0000

    Remove contextual menus, add left click nav, form cleanup.

--- js/jstree.js
+++ js/jstree.js
@@ -12,27 +12,14 @@ Drupal.settings.jsTree_jQuery = jQuery.noConflict(true);
                     'data': settings.islandora_serial_object.data
                 },
                 'types': settings.islandora_serial_object.types,
-                'plugins' : settings.islandora_serial_object.plugins,
-                'contextmenu' : {
-                    'items': function (node) {
-                        if (node.type != 'stub') {
-                            return {
-                                'Goto': {
-                                    'label': Drupal.t('Go to'),
-                                    'action': function (obj) {
-                                        if (node.type == 'pdf') {
-                                           window.location = Drupal.settings.basePath + 'islandora/object/' + node.id + '/datastream/OBJ/view/', 'Islandora Object', 'window settings';
-                                        }
-                                        else {
-                                           window.location = Drupal.settings.basePath + 'islandora/object/' + node.id, 'Islandora Object', 'window settings';
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        else {
-                            return {}
-                        }
+                'plugins' : settings.islandora_serial_object.plugins
+            });
+            $('#islandora_serial_object_tree_view').bind("select_node.jstree", function(node, selected, event) {
+                // For some reason jsTree doesn't pass the click through
+                // in the event parameter, so grab it from selected instead.
+                if (typeof selected.event != 'undefined') {
+                    if (selected.event.type == 'click') {
+                        window.location = Drupal.settings.basePath + 'islandora/object/' + selected.node.id;
                     }
                 }
             });
